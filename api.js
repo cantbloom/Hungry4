3d8e2c7db05fc6227e22c86742ded19f940f7ec6
Joshua Blum <joshblum@mit.edu>
2012-06-09 10:47:28 -0700

    yelp + oauth

---
+++ api.js
@@ -0,0 +1,8 @@
+/////////////YELP API/////////////////////
+//V1.0
+var YWSID = "bsDplBO-q-nw4JzuS-f4Xw"
+//V2.0
+var consumer_key = "ZtNvNaskZi5sZhw1DpRN2g"
+var consumer_secret = "Ezd5O9QIXiHzcVSy7QrVqjkzTv4"
+var token = "evQDUZTMYQRLcmHsKcAR4BGNKCseYpVf"
+var token_secret = "5wgCHh419h4RCUQKCGU1K49dar4"
